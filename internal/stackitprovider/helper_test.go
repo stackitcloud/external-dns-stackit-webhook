e734d688f5bb27c1263b865e27474f5dce58234c
Stijn IJzermans <stijn@eidp.com>
2026-08-03 09:01:14 +0200

    fix: add lint ignore

--- internal/stackitprovider/helper_test.go
+++ internal/stackitprovider/helper_test.go
@@ -10,6 +10,7 @@ import (
 	"sigs.k8s.io/external-dns/endpoint"
 )
 
+//nolint:gocognit // keeps one end-to-end behavior test in a single function
 func TestModifyChange(t *testing.T) {
 	t.Parallel()
 
